9b4f9024db0b5f64534761e9f9c3bb16d355c2d8
Sankrub <navathon.l@ku.th>
2024-03-26 18:53:05 +0700

    Initial Commit

---
+++ WongnaiApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WongnaiApp1
+//
+//  Created by Navathon Limamapar on 26/3/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
